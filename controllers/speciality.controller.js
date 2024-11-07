0ea59c3e8a64da519c1135ececbe8c18bc79e404
hieu <phananhhieu2002@gmail.com>
2024-11-07 21:41:16 +0700

    get all specialities

--- controllers/speciality.controller.js
+++ controllers/speciality.controller.js
@@ -1,5 +1,28 @@
 const Speciality = require("../models/speciality.model");
 
+const defaultSize = 10;
+
+const getAllSpeciality = async (data,req,res,next) => {
+    const {size, page} = req.body;
+    const limit = size ? size : defaultSize;
+    const offset = page ? (page - 1) * limit : 0;
+    const result = await Speciality.findAll({
+        limit: limit,
+        offset: offset
+    })
+    if(!result){
+        res.status(500).json({
+            result: 0,
+            message: "Error , Don't get specialities"
+        })
+    }
+    res.status(200).json({
+        result: 1,
+        message: "Get all specialities successfully",
+        data: result
+    })
+}
+
 const createSpeciality = async (req,res) => {
     const request = {
         name: req.body.name,
@@ -20,4 +43,7 @@ const createSpeciality = async (req,res) => {
     })
 }
 
-module.exports = createSpeciality;
+module.exports = {
+    getAllSpeciality,
+    createSpeciality
+};
--- route/speciality.route.js
+++ route/speciality.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const SpecialityRouter = express.Router();
+const isLogInController = require("../controllers/is-logIn.controller");
 const isAdminController = require("../controllers/is-admin.controller");
-const createSpeciality = require("../controllers/speciality.controller");
+const {getAllSpeciality,createSpeciality} = require("../controllers/speciality.controller");
 
+SpecialityRouter.get("/",isLogInController,getAllSpeciality);
 SpecialityRouter.post("/",isAdminController,createSpeciality);
-
 
 module.exports = SpecialityRouter;
