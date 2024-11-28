8d7db16e6f6e1c95e5d4ec225aff5a589293025c
hieu <phananhhieu2002@gmail.com>
2024-11-28 22:34:43 +0700

    permission admin-doctor-supporter

--- controllers/is-doctor.controller.js
+++ controllers/is-doctor.controller.js
@@ -3,10 +3,11 @@ const isDoctorController = (req, res, next) => {
   const access_token = req.headers["authorization"];
   const token = access_token.split(" ")[1];
   const payload = jwt.verify(token, process.env.JWT_SECRET);
-  if (!payload.hasOwnProperty("doctor") || payload.doctor.role !== "doctor") {
+  if (!payload.hasOwnProperty("doctor")
+      || (payload.doctor.role !== "doctor" && payload.doctor.role !== "admin")) {
     res.status(400).json({
       result: 0,
-      msg: "This account not be doctor",
+      msg: "This account not be doctor , you don't allow to do this action",
     });
   }
   next();
--- controllers/is-supporter.controller.js
+++ controllers/is-supporter.controller.js
@@ -3,7 +3,8 @@ const IsSupporterController = (req,res) => {
     const access_token = req.headers["authorization"];
     const token = access_token.split(" ")[1];
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-    if (!payload.hasOwnProperty("doctor") || payload.doctor.role !== "supporter") {
+    if (!payload.hasOwnProperty("doctor")
+        || (payload.doctor.role !== "supporter" && payload.doctor.role !== "admin")) {
         res.status(400).json({
             result: 0,
             msg: "This account not be supporter",
