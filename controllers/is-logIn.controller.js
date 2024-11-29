bdaa86e4a431f1f52dcfaac2da00e30e390d477f
hieu <phananhhieu2002@gmail.com>
2024-11-30 00:05:45 +0700

    fix verify log in

--- controllers/is-logIn.controller.js
+++ controllers/is-logIn.controller.js
@@ -1,12 +1,12 @@
 const isLogInController = (req, res, next) => {
   const access_token = req.headers["authorization"];
-  if (!access_token) {
+  const token_data = access_token.split(" ")[1];
+  if (!token_data || token_data === "null") {
     return res.status(400).json({
       result: 0,
       msg: "Not logged in"
     });
   }
-  const token_data = access_token.split(" ")[1];
   next(token_data);
 };
 module.exports = isLogInController;
