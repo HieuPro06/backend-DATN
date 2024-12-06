7d1b8cb914a7ef59321595ca40369861f466bd6d
hieu <phananhhieu2002@gmail.com>
2024-12-06 17:24:25 +0700

    get doctor by speciality & service

--- route/doctor.routes.js
+++ route/doctor.routes.js
@@ -6,6 +6,8 @@ const {
   createDoctor,
   updateDoctor,
   deleteDoctor,
+  getAllDoctorsBySpecialityId,
+  getAllDoctorsByServiceId
 } = require("../controllers/doctor.controller.js");
 const isLogInController = require("../controllers/is-logIn.controller");
 const isAdminController = require("../controllers/is-admin.controller");
@@ -15,5 +17,6 @@ DoctorRouter.get("/:id",isLogInController,getDoctorByID);
 DoctorRouter.post("/",isAdminController,createDoctor);
 DoctorRouter.put("/:id",isAdminController,updateDoctor);
 DoctorRouter.delete("/:id",isAdminController,deleteDoctor);
-
+DoctorRouter.get("/speciality/:id",isLogInController,getAllDoctorsBySpecialityId);
+DoctorRouter.get("/service/:id",isLogInController,getAllDoctorsByServiceId);
 module.exports = DoctorRouter;
