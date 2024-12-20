86763d36fa9e2336609e8db822564dadaeb35fba
kien3052002 <kien3052002@gmail.com>
2024-12-20 16:08:58 +0700

    bugn fixes

--- controllers/change-avatar-patient.controller.js
+++ controllers/change-avatar-patient.controller.js
@@ -7,7 +7,9 @@ const changeAvatarPatientController = async (data, req, res, next) => {
   try {
     const payload = jwt.decode(data);
     const result = await Patient.update(
-      { avatar: `${process.env.ENV_DEVELOPMENT}/Patient/${req.file.filename}` },
+      {
+        avatar: `${process.env.ENV_DEVELOPMENT}/Patient/${req.file?.filename}`,
+      },
       {
         where: { id: payload.patient.id },
       }
